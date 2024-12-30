1330654b47d8eeaa62355ab9a26ba098318abd37
Sadixo <manuelgusella13@gmail.com>
2024-12-30 12:27:43 +0100

    Modifica file wordchanger e messo i G a pedice nel documento fino ad ora

--- RTB/wordchanger.py
+++ RTB/wordchanger.py
@@ -1,6 +1,51 @@
 import re #libreria per lavorare con le sottostringhe
 
-parole=["Analisi-dei-Requisiti","baseline","branch","docker","ia","issue","latex","LLM","merge","milestone","PR","retrospettiva","review","RTB","SAL","sprint","Synclab"]
+def add_g(testo,parola):
+    sostituto=parola+"$_G$"
+    pattern=r'(?<!-)\b'+re.escape(parola)+r'\b(?![\w$-])'
+    testo=re.sub(pattern,sostituto,testo,flags=re.IGNORECASE)
+
+    return testo
+
+def add_g_comp(testo,parola):
+    termine='-'.join(parola.split())
+    sostituto=termine+"$_G$"
+    pattern=r'(?<!-)\b'+re.escape(parola)+r'\b(?![\w$-])'
+    testo=re.sub(pattern,sostituto,testo,flags=re.IGNORECASE)
+
+    return testo
+    
+
+parole=[
+    "Agile","API","Attore",
+    "BAC","Baseline","Branch","Build",
+    "Capitolato","ClickHouse","Committente","Container","Cruscotto","CI",
+    "Dashboard","Database","DBMS","Documentazione","Docker",
+    "EAC","efficacia","efficienza","ETC",
+    "faker","framework","feedback",
+    "Git","GitHub",
+    "IA","IEEE","Issue","ITS",
+    "Kafka",
+    "latex","LLM",
+    "Merge","Milestone","MVP",
+    "Norme","Overleaf",
+    "PB","PoC","Progetto","PR","Proponente","Protocollo","Python",
+    "Query",
+    "Repository","Requisito","Requisiti","RTB",
+    "SAL","Scrum","Sprint","SQL","Stakeholder","Synclab",
+    "Teamwork","Test",
+    "UML",
+    "Versionamento"
+]
+
+parole_composte=[
+    "Analisi dei Requisiti",
+    "Best Practices",
+    "Caso d'uso","Casi d'uso","Ciclo di vita","Continuous Integration",
+    "Sprint retrospective","Sprint review",
+    "User Story",
+    "Way of Working"
+]
 
 with open("Analisi_dei_Requisiti.tex","r+") as file:
 
@@ -27,11 +72,10 @@ with open("Analisi_dei_Requisiti.tex","r+") as file:
 
     #con regex
     for linea in linee:
+        for parola in parole_composte:
+            linea=add_g_comp(linea,parola)
         for parola in parole:
-            p=parola+"$_G$"
-            pattern=r'\b'+re.escape(parola)+r'\b(?![\w$])'
-            #re.findall(pattern,linea,flags=re.IGNORECASE)
-            linea=re.sub(pattern,p,linea,flags=re.IGNORECASE)
+            linea=add_g(linea,parola)
         nuovo_contenuto=nuovo_contenuto+linea
     #print(nuovo_contenuto)
 
