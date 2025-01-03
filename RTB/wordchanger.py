2af7848b33f12d62ad6a67a29616791fdeeda30b
Sadixo <manuelgusella13@gmail.com>
2025-01-03 10:11:14 +0100

    Modifica finale al file wordchanger
    Ultima modifica per far iniziare l'aggiunta dei G a pedice dopo l'end
    del titlepage

--- RTB/wordchanger.py
+++ RTB/wordchanger.py
@@ -1,19 +1,21 @@
 import re #libreria per lavorare con le sottostringhe
 
-def add_g(testo,parola):
-    sostituto=parola+"$_G$"
-    pattern=r'(?<!-)\b'+re.escape(parola)+r'\b(?![\w$-])'
+def change_word(testo,parola,sostituto):
+    pattern=r'(?<!\S)\b'+re.escape(parola)+r'\b(?![\w$-])'
     testo=re.sub(pattern,sostituto,testo,flags=re.IGNORECASE)
 
     return testo
+
+def add_g(testo,parola):
+    sostituto=parola+"$_G$"
+
+    return change_word(testo,parola,sostituto)
 
 def add_g_comp(testo,parola):
     termine='-'.join(parola.split())
     sostituto=termine+"$_G$"
-    pattern=r'(?<!-)\b'+re.escape(parola)+r'\b(?![\w$-])'
-    testo=re.sub(pattern,sostituto,testo,flags=re.IGNORECASE)
 
-    return testo
+    return change_word(testo,parola,sostituto)
     
 
 parole=[
@@ -52,30 +54,19 @@ with open("Analisi_dei_Requisiti.tex","r+") as file:
     linee=file.readlines()
     nuovo_contenuto=""
 
-    #senza regex
-    #legge ogni linea
-    #for linea in linee:
-    #    #legge ogni parola della linea
-    #    for word in linea.split():
-    #        #ripeto questo ciclo per ogni parola presente nella lista parole
-    #        for parola in parole:
-    #            p=parola+"$_G$"
-    #            pattern=r'\b'+re.escape(p)+r'.*'
-    #            #controlla se la parola è presente nella word che sta leggendo
-    #            if parola in word:
-    #                #se è presente controlla se ha la G a pedice e se non la ha la inserisce
-    #                if p not in word:
-    #                    print(word)
-    #                    word=word.replace(parola,parola+"$_G$")
-    #        nuovo_contenuto=nuovo_contenuto+" "+word
-    #    nuovo_contenuto=nuovo_contenuto+"\n"
-
-    #con regex
+    #variabile booleana che serve a capire quando iniziare a mettere i G a pedice.
+    #noi vogliamo che li metta dopo la titlepage
+    start=False
     for linea in linee:
-        for parola in parole_composte:
-            linea=add_g_comp(linea,parola)
-        for parola in parole:
-            linea=add_g(linea,parola)
+        if(linea=="\\end{titlepage}\n"):
+            start=True
+        
+        if(start==True):
+            for parola in parole_composte:
+                linea=add_g_comp(linea,parola)
+
+            for parola in parole:
+                linea=add_g(linea,parola)
         nuovo_contenuto=nuovo_contenuto+linea
     #print(nuovo_contenuto)
 
